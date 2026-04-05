8d20979276f69c15821f2f25afea8b076b0f2ae1
AngadOnTop <angadjotd@gmail.com>
2026-04-05 17:00:22 +1000

    removed popup

--- src/useStore.js
+++ src/useStore.js
@@ -104,15 +104,12 @@ export function useStore() {
   }, [subjects.length, setSubjects, setTasks])
 
   const deleteSubject = useCallback((id) => {
-    const confirmed = window.confirm(`Are you sure you want to delete this subject? All tasks will be removed.`)
-    if (confirmed) {
-      setSubjects(prev => prev.filter(s => s.id !== id))
-      setTasks(prev => {
-        const next = { ...prev }
-        delete next[id]
-        return next
-      })
-    }
+    setSubjects(prev => prev.filter(s => s.id !== id))
+    setTasks(prev => {
+      const next = { ...prev }
+      delete next[id]
+      return next
+    })
   }, [setSubjects, setTasks])
 
   const toggleExpand = useCallback((sid) => {
